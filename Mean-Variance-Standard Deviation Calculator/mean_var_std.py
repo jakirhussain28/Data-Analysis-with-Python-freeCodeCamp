7759d8ac24b3a3e15874c2794cb1d56739ce17c0
Zakir Hussain <99417766+jakirhussain28@users.noreply.github.com>
2025-06-22 15:40:17 +0530

    Update mean_var_std.py

--- Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# Create a function named calculate() in mean_var_std.py that uses Numpy to output 
+# the mean, variance, standard deviation, max, min, and sum of the 
+# rows, columns, and elements in a 3 x 3 matrix.
+
 def calculate(list):
     if len(list) != 9:
         raise ValueError("List must contain nine numbers.")
