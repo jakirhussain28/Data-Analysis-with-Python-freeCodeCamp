65ca904089dede1e1f5b7892a0a48a1c21c16eba
Zakir Hussain <99417766+jakirhussain28@users.noreply.github.com>
2025-06-22 15:41:00 +0530

    Update mean_var_std.py

--- Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def calculate(list):
     if len(list) != 9:
-        raise ValueError("List must contain nine numbers.")
+        raise ValueError("only 9 nums to be exist.")
     
     matrix = np.array(list).reshape(3, 3)
     
